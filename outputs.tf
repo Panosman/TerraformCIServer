9d39dd30bf033fae563b2a1fcab95845b4176ab4
pmandrekas <panman97@hotmail.com>
2023-04-24 16:35:24 +0300

    changed outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "public_ip" {
   value = azurerm_public_ip.main.ip_address
 }
 
+output "username" {
+  value = var.admin_username
+}
+
 output "tls_private_key" {
   value     = tls_private_key.ciserver_ssh.private_key_pem
   sensitive = true
